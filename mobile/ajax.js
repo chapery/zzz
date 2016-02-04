24f200cac2537aa81f238b9b664894d1580ae94e
chapery <2474301702@qq.com>
2016-02-04 11:20:19 +0800

    tablet

--- mobile/ajax.js
+++
@@ -1,9 +0,0 @@
-function createXHR(){
-	if(typeof XMLHttpRequest != "undefined"){
-		return new XMLHttpRequest();
-	}else if(typeof ActiveXObject != "undefined"){
-		return new ActiveXObject();
-	}else{
-		throw new Error("no XHR object available.");
-	}
-};
