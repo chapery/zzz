733d002499d3e353c52e5d4bc5508f627f27ada7
chapery <2474301702@qq.com>
2016-07-29 17:18:12 +0800

    slide.html prettify and requireJs/main.js

--- requireJs/js/main.js
+++ requireJs/js/main.js
@@ -1,10 +1,10 @@
 require.config({
-	baseUrl:'js',
+	baseUrl:'js',//默认为main.js所在的目录
 	paths:{
-		jquery:'../../jquery/jquery'
+		jquery:["http://libs.baidu.com/jquery/2.0.3/jquery", '../../jquery/jquery']//远程加载不成功切换到本地
 	}
 })
-require(['require','./shirt','jquery'],function(require,shirt,jquery){
+require(['require','./shirt','jquery'],function(require,shirt,$){
 	console.log(shirt);
 	console.log($);
 });
